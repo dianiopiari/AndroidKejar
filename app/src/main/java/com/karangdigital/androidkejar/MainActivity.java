4c85e9e1871c98deb5ce48ef34e73aaec2931b8a
Diani Opiari <dianiopiari89@gmail.com>
2017-08-06 14:32:28 +0800

    Initial commit

---
+++ app/src/main/java/com/karangdigital/androidkejar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.karangdigital.androidkejar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
